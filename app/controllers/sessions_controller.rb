3d699b2efc8cc8c30b21038babb397b1854ac001
Mohammed Noori <kmanoorib@gmail.com>
2019-07-16 03:11:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    session[:name] = params[:name] # sets session[:name] if :name was given
+    # session[:name] = params[:name] # sets session[:name] if :name was given
+    if params[:name]
+      session[:name] = params[:name]
+    else
+      
+    end
   end
 end
