6ac28fd6160166627d1061ed5801188ad634872a
Mohammed Noori <kmanoorib@gmail.com>
2019-07-16 03:16:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     # session[:name] = params[:name] # sets session[:name] if :name was given
-    if params[:name]
+    if params[:name] && !params[:name].empty?
       session[:name] = params[:name] # sets session[:name] if :name was given
     else
       redirect_to action: "new" # could be written as redirect_to(action: 'new') as well
