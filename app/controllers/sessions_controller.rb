bf9a2e26565262eaaff7a8aff330a021e018384d
Mohammed Noori <kmanoorib@gmail.com>
2019-07-16 03:15:10 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     if params[:name]
       session[:name] = params[:name] # sets session[:name] if :name was given
     else
-      redirect_to new_url
+      redirect_to action: "new"
     end
   end
   
